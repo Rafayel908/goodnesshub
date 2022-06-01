1b02b37e01507196126ccc2d6ecf20cad06a3072
Rafayel908 <r.mayilyan@codeex.io>
2022-06-01 14:51:28 +0400

    created home component, module for lazyLoading and routing

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -5,13 +5,15 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import {NgbRatingModule} from "@ng-bootstrap/ng-bootstrap";
 import {SharedModule} from "../shared/shared.module";
 import {FormsModule} from "@angular/forms";
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 
 @NgModule({
   declarations: [
     ShopComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
